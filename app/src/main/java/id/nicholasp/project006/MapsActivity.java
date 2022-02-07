3d7ade193c9688792e03539e8c03f6659a0e42dc
nicholas-phandinata <nicholas99.p@gmail.com>
2022-02-07 15:55:18 +0700

    Upload project 006 hw to github

---
+++ app/src/main/java/id/nicholasp/project006/MapsActivity.java
@@ -0,0 +1,40 @@
+package id.nicholasp.project006;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng maybank = new LatLng(-6.226956267814199, 106.7995667543529);
+        mMap.addMarker(new MarkerOptions().position(maybank).title("Marker in Maybank"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(maybank, 15));
+
+    }
+}
